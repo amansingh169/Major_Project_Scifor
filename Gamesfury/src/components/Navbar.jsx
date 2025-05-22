af23eddb5a4312886904d6f394e8bf65aadb21a8
Aman Singh <as2211075@gmail.com>
2025-05-23 00:34:33 +0530

    Created Vite App

---
+++ Gamesfury/src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div>
+      <nav>
+        <a href="/">Home</a>
+        <a href="/">About</a>
+        <a href="/">Contact Us</a>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
