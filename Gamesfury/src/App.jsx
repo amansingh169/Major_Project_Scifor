6917a64d91c135360966e8e6ee17f006608b3d7a
Aman Singh <as2211075@gmail.com>
2025-05-24 17:47:53 +0530

    Added Theme System
    In this commit I added a theme context and a theme selector with multiple themes for my project, such as light, ash, dark, onyx, neon and fire.

--- Gamesfury/src/App.jsx
+++ Gamesfury/src/App.jsx
@@ -1,13 +1,17 @@
 import { useState } from "react";
 
 import Navbar from "./components/Navbar";
+import ThemeSelector from "./components/ThemeSelector";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <Navbar />
+      <div className="container my-5">
+        <Navbar />
+        <ThemeSelector />
+      </div>
     </>
   );
 }
--- Gamesfury/src/components/Navbar.jsx
+++ Gamesfury/src/components/Navbar.jsx
@@ -1,19 +1,36 @@
+import cardImage from "../assets/neon.jpg";
+
 const Navbar = () => {
   return (
-    <div>
-      <nav>
-        <a className="a" href="/">
-          Home
-        </a>
-        <a href="/">About</a>
-        <a href="/">Contact Us</a>
-        <button className="btn btn-warning">What's up?</button>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, repellat perspiciatis
-          repudiandae cupiditate ipsam officiis consectetur explicabo id accusantium animi.
-        </p>
-      </nav>
-    </div>
+    <nav>
+      <a className="a" href="/">
+        Home
+      </a>
+      <a href="/">About</a>
+      <a href="/">Contact Us</a>
+      <button className="btn btn-primary">What's up?</button>
+      <button className="btn btn-secondary">Nothing</button>
+
+      <div className="card" style={{ width: "18rem", "margin-top": "2rem" }}>
+        <img src={cardImage} className="card-img-top" />
+        <div className="card-body">
+          <h5 className="card-title">Lorem ipsum dolor sit amet.</h5>
+          <p className="card-text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea tempora temporibus beatae
+            ab. Facere enim modi provident nostrum dolore placeat.
+          </p>
+          <div className="d-flex justify-content-between">
+            <button className="btn btn-primary">Buy Now</button>
+            <button className="btn btn-secondary">Add to cart</button>
+          </div>
+        </div>
+      </div>
+
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, repellat perspiciatis
+        repudiandae cupiditate ipsam officiis consectetur explicabo id accusantium animi.
+      </p>
+    </nav>
   );
 };
 
