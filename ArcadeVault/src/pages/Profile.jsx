340c7f99ae734beeb19d798703991769cbdc4133
Aman Singh <as2211075@gmail.com>
2025-06-25 18:47:41 +0530

    Added user information in profile section

--- ArcadeVault/src/pages/Profile.jsx
+++ ArcadeVault/src/pages/Profile.jsx
@@ -3,7 +3,7 @@ import ThemeSelector from "../components/ThemeSelector";
 import { UserContext } from "../contexts/UserContext";
 
 const Profile = () => {
-  const { user, setUser } = useContext(UserContext);
+  const { user } = useContext(UserContext);
 
   return (
     <div className="profile-wrapper">
@@ -14,9 +14,24 @@ const Profile = () => {
         </a>
       </div>
 
-      <div className="settings">
-        <div className="text-primary">
-          <div className="fs-4 mb-2">Preferred Theme:</div>
+      <div className="settings d-flex flex-column gap-3 align-items-start">
+        <div>
+          <span className="fw-semibold fs-6">DISPLAY NAME:</span>
+          <div className="bg-card px-3 py-1 rounded mt-1 fs-5 text-primary">{user.name}</div>
+        </div>
+
+        <div>
+          <span className="fw-semibold fs-6">USERNAME:</span>
+          <div className="bg-card px-3 py-1 rounded mt-1 fs-5 text-primary">#{user.username}</div>
+        </div>
+
+        <div>
+          <span className="fw-semibold fs-6">EMAIL:</span>
+          <div className="bg-card px-3 py-1 rounded mt-1 fs-5 text-primary">{user.email}</div>
+        </div>
+
+        <div>
+          <div className="fw-semibold fs-6">PREFERRED THEME:</div>
           <ThemeSelector />
         </div>
       </div>
